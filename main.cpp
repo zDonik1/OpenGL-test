5873ed69794925d3a24891fc9fb04e30aa04ee9f
Doniyor Tokhirov <tokhirovdoniyor@gmail.com>
2020-07-17 10:34:57 +0500

    Geometry shader

--- main.cpp
+++ main.cpp
@@ -1,497 +1,94 @@
 // Headers
 #include <iostream>
-#include <chrono>
 #include <GL/glew.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <SOIL/SOIL.h>
 #include <SFML/Window.hpp>
 
 // Shader sources
-const GLchar* sceneVertexSource = R"glsl(
+const GLchar* vertexSource = R"glsl(
     #version 150 core
-    in vec3 position;
-    in vec3 color;
-    in vec2 texcoord;
-    out vec3 Color;
-    out vec2 Texcoord;
-    uniform mat4 model;
-    uniform mat4 view;
-    uniform mat4 proj;
-    uniform vec3 overrideColor;
-    void main()
-    {
-        Color = overrideColor * color;
-        Texcoord = texcoord;
-        gl_Position = proj * view * model * vec4(position, 1.0);
-    }
-)glsl";
 
-const GLchar* sceneFragmentSource = R"glsl(
-    #version 150 core
-    in vec3 Color;
-    in vec2 Texcoord;
-    out vec4 outColor;
-    uniform sampler2D texKitten;
-    uniform sampler2D texPuppy;
-    void main()
-    {
-        outColor = vec4(Color, 1.0) * mix(texture(texKitten, Texcoord), texture(texPuppy, Texcoord), 0.5);
-    }
-)glsl";
+    in float inValue;
 
-const GLchar* screenVertexSource = R"glsl(
-    #version 150 core
-    in vec2 position;
-    in vec2 texcoord;
-    out vec2 Texcoord;
-    void main()
-    {
-        Texcoord = texcoord;
-        gl_Position = vec4(position, 0.0, 1.0);
-    }
-)glsl";
-
-// weightSum is not optimized btw
-const GLchar* screenFragmentSource = R"glsl(
-    #version 150 core
-    in vec2 Texcoord;
-    out vec4 outColor;
-    uniform sampler2D texFramebuffer;
-
-    const float standardDeviation = 2.0;
-    const float squareSd = standardDeviation * standardDeviation;
+    out float outValue;
 
     void main()
     {
-        vec4 sum = vec4(0.0);
-        float weightSum = 0.0;
-        float weight = 0.0;
-        for (int i = -4; i <= 4; ++i) {
-            weight = pow(2.7182, -0.5 * (i * i) / squareSd);
-            sum += texture(texFramebuffer, vec2(Texcoord.x + i * 1.0 / 300.0, Texcoord.y)) * weight;
-            weightSum += weight;
-        }
-        outColor = sum / weightSum;
+        outValue = sqrt(inValue);
     }
 )glsl";
 
-// weightSum is not optimized btw
-const GLchar* gaussBlurFragmentSource = R"glsl(
-    #version 150 core
-    in vec2 Texcoord;
-    out vec4 outColor;
-    uniform sampler2D texFramebuffer;
-
-    const float standardDeviation = 2.0;
-    const float squareSd = standardDeviation * standardDeviation;
-
-    void main()
-    {
-        vec4 sum = vec4(0.0);
-        float weightSum = 0.0;
-        float weight = 0.0;
-        for (int i = -4; i <= 4; ++i) {
-            weight = pow(2.7182, -0.5 * (i * i) / squareSd);
-            sum += texture(texFramebuffer, vec2(Texcoord.x, Texcoord.y + i * 1.0 / 300.0)) * weight;
-            weightSum += weight;
-        }
-        outColor = sum / weightSum;
-    }
-)glsl";
-
-// Cube vertices
-GLfloat cubeVertices[] = {
-    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-
-    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-
-    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-
-     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-
-    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-     0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-     0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-    -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-    -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-
-    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-     0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f,
-     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-     0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f,
-    -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f,
-    -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f,
-
-    -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
-     1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-     1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-     1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
-    -1.0f,  1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f,
-    -1.0f, -1.0f, -0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f
-};
-
-// Quad vertices
-GLfloat quadVertices[] = {
-    -1.0f,  1.0f,  0.0f, 1.0f,
-     1.0f,  1.0f,  1.0f, 1.0f,
-     1.0f, -1.0f,  1.0f, 0.0f,
-
-     1.0f, -1.0f,  1.0f, 0.0f,
-    -1.0f, -1.0f,  0.0f, 0.0f,
-    -1.0f,  1.0f,  0.0f, 1.0f
-};
-
-// Create a texture from an image file
-GLuint loadTexture(const GLchar* path)
-{
-    GLuint texture;
-    glGenTextures(1, &texture);
-
-    int width, height;
-    unsigned char* image;
-
-    glBindTexture(GL_TEXTURE_2D, texture);
-    image = SOIL_load_image(path, &width, &height, 0, SOIL_LOAD_RGB);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
-    SOIL_free_image_data(image);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    return texture;
-}
-
-void createShaderProgram(const GLchar* vertSrc, const GLchar* fragSrc, GLuint& vertexShader, GLuint& fragmentShader, GLuint& shaderProgram)
-{
-    // Create and compile the vertex shader
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertSrc, NULL);
-    glCompileShader(vertexShader);
-    GLint success;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (success == GL_FALSE) {
-        char buffer[512];
-        glGetShaderInfoLog(vertexShader, 512, NULL, buffer);
-        std::cout << "vertex shader failed: " << buffer;
-    }
-
-    // Create and compile the fragment shader
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragSrc, NULL);
-    glCompileShader(fragmentShader);
-    if (success == GL_FALSE) {
-        char buffer[512];
-        glGetShaderInfoLog(fragmentShader, 512, NULL, buffer);
-        std::cout << "fragment shader failed: " << buffer;
-    }
-
-    // Link the vertex and fragment shader into a shader program
-    shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glBindFragDataLocation(shaderProgram, 0, "outColor");
-    glLinkProgram(shaderProgram);
-}
-
-void createFramebuffer(GLuint &frameBuffer, GLuint &texColorBuffer, GLuint &rboDepthStencil)
-{
-    // Create framebuffer
-    glGenFramebuffers(1, &frameBuffer);
-    glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer);
-
-    // Create texture to hold color buffer
-    glGenTextures(1, &texColorBuffer);
-    glBindTexture(GL_TEXTURE_2D, texColorBuffer);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
-
-    // Create Renderbuffer Object to hold depth and stencil buffers
-    glGenRenderbuffers(1, &rboDepthStencil);
-    glBindRenderbuffer(GL_RENDERBUFFER, rboDepthStencil);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, 800, 600);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, rboDepthStencil);
-}
-
-void specifySceneVertexAttributes(GLuint shaderProgram)
-{
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), 0);
-
-    GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
-    glEnableVertexAttribArray(colAttrib);
-    glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(3 * sizeof(GLfloat)));
-
-    GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
-    glEnableVertexAttribArray(texAttrib);
-    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat), (void*)(6 * sizeof(GLfloat)));
-}
-
-void specifyScreenVertexAttributes(GLuint shaderProgram)
-{
-    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-    glEnableVertexAttribArray(posAttrib);
-    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
-
-    GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
-    glEnableVertexAttribArray(texAttrib);
-    glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
-}
+GLfloat values[] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
 
 int main()
 {
-    auto t_start = std::chrono::high_resolution_clock::now();
-
-    sf::ContextSettings settings;
-    settings.depthBits = 24;
-    settings.stencilBits = 8;
-    settings.majorVersion = 3;
-    settings.minorVersion = 2;
-
-    sf::Window window(sf::VideoMode(800, 600, 32), "OpenGL", sf::Style::Titlebar | sf::Style::Close, settings);
+    sf::Context context;
 
     // Initialize GLEW
     glewExperimental = GL_TRUE;
     glewInit();
 
     // Create VAOs
-    GLuint vaoCube, vaoQuad;
-    glGenVertexArrays(1, &vaoCube);
-    glGenVertexArrays(1, &vaoQuad);
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
 
-    // Load vertex data
-    GLuint vboCube, vboQuad;
-    glGenBuffers(1, &vboCube);
-    glGenBuffers(1, &vboQuad);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vboCube);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ARRAY_BUFFER, vboQuad);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    // Load values
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(values), values, GL_STATIC_DRAW);
 
     // Create shader programs
-    GLuint sceneVertexShader, sceneFragmentShader, sceneShaderProgram;
-    createShaderProgram(sceneVertexSource, sceneFragmentSource,
-                        sceneVertexShader, sceneFragmentShader, sceneShaderProgram);
+    GLuint shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(shader, 1, &vertexSource, nullptr);
+    glCompileShader(shader);
 
-    GLuint gaussBlurVertexShader, gaussBlurFragmentShader, gaussBlurShaderProgram;
-    createShaderProgram(screenVertexSource, gaussBlurFragmentSource,
-                        gaussBlurVertexShader, gaussBlurFragmentShader, gaussBlurShaderProgram);
+    GLuint program = glCreateProgram();
+    glAttachShader(program, shader);
 
-    GLuint screenVertexShader, screenFragmentShader, screenShaderProgram;
-    createShaderProgram(screenVertexSource, screenFragmentSource,
-                        screenVertexShader, screenFragmentShader, screenShaderProgram);
+    const GLchar *feedbackVaryings[] = { "outValue" };
+    glTransformFeedbackVaryings(program, 1, feedbackVaryings, GL_INTERLEAVED_ATTRIBS);
 
-    // Specify the layout of the vertex data
-    glBindVertexArray(vaoCube);
-    glBindBuffer(GL_ARRAY_BUFFER, vboCube);
-    specifySceneVertexAttributes(sceneShaderProgram);
+    glLinkProgram(program);
+    glUseProgram(program);
 
-    glBindVertexArray(vaoQuad);
-    glBindBuffer(GL_ARRAY_BUFFER, vboQuad);
-    specifyScreenVertexAttributes(screenShaderProgram);
+    // Specify vertex attribs
+    GLint inputAttrib = glGetAttribLocation(program, "inValue");
+    glEnableVertexAttribArray(inputAttrib);
+    glVertexAttribPointer(inputAttrib, 1, GL_FLOAT, GL_FALSE, 0, 0);
 
-    // Load textures
-    GLuint texKitten = loadTexture("sample.png");
-    GLuint texPuppy = loadTexture("sample2.png");
+    // Create output buffer
+    GLuint tbo;
+    glGenBuffers(1, &tbo);
+    glBindBuffer(GL_ARRAY_BUFFER, tbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(values), nullptr, GL_STATIC_READ);
 
-    glUseProgram(sceneShaderProgram);
-    glUniform1i(glGetUniformLocation(sceneShaderProgram, "texKitten"), 0);
-    glUniform1i(glGetUniformLocation(sceneShaderProgram, "texPuppy"), 1);
+    glEnable(GL_RASTERIZER_DISCARD);
 
-    glUseProgram(screenShaderProgram);
-    glUniform1i(glGetUniformLocation(screenShaderProgram, "texFramebuffer"), 0);
+    glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, tbo);
 
-    GLint uniModel = glGetUniformLocation(sceneShaderProgram, "model");
+    // Run shaders
+    glBeginTransformFeedback(GL_POINTS);
+    glDrawArrays(GL_POINTS, 0, 5);
+    glEndTransformFeedback();
+    glFlush();
 
-    // create framebuffers
-    GLuint sceneRenderFrameBuffer, sceneRenderTexColorBuffer, sceneRenderRboDepthStencil;
-    GLuint gaussBlurFrameBuffer, gaussBlurTexColorBuffer, gaussBlurRboDepthStencil;
-    createFramebuffer(sceneRenderFrameBuffer, sceneRenderTexColorBuffer,
-                      sceneRenderRboDepthStencil);
-    createFramebuffer(gaussBlurFrameBuffer, gaussBlurTexColorBuffer,
-                      gaussBlurRboDepthStencil);
+    GLfloat feedback[5];
+    glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, sizeof(feedback), feedback);
 
-    // Set up projuection
-    glm::mat4 view = glm::lookAt(
-        glm::vec3(2.5f, 2.5f, 2.0f),
-        glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3(0.0f, 0.0f, 1.0f)
-    );
-
-    glUseProgram(sceneShaderProgram);
-
-    GLint uniView = glGetUniformLocation(sceneShaderProgram, "view");
-    glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
-
-    glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 1.0f, 10.0f);
-    GLint uniProj = glGetUniformLocation(sceneShaderProgram, "proj");
-    glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
-
-    GLint uniColor = glGetUniformLocation(sceneShaderProgram, "overrideColor");
-
-    bool running = true;
-    while (running)
-    {
-        sf::Event windowEvent;
-        while (window.pollEvent(windowEvent))
-        {
-            switch (windowEvent.type)
-            {
-            case sf::Event::Closed:
-                running = false;
-                break;
-
-            default:
-                break;
-            }
-        }
-
-        // Bind our framebuffer and draw 3D scene (spinning cube)
-        glBindFramebuffer(GL_FRAMEBUFFER, sceneRenderFrameBuffer);
-        glBindVertexArray(vaoCube);
-        glEnable(GL_DEPTH_TEST);
-        glUseProgram(sceneShaderProgram);
-
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texKitten);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texPuppy);
-
-        // Clear the screen to white
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        // Calculate transformation
-        auto t_now = std::chrono::high_resolution_clock::now();
-        float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
-
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(
-            model,
-            time * glm::radians(180.0f),
-            glm::vec3(0.0f, 0.0f, 1.0f)
-        );
-        glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
-
-        // Draw cube
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-        glEnable(GL_STENCIL_TEST);
-
-            // Draw floor
-            glStencilFunc(GL_ALWAYS, 1, 0xFF);
-            glStencilOp(GL_KEEP, GL_KEEP, GL_REPLACE);
-            glStencilMask(0xFF);
-            glDepthMask(GL_FALSE);
-            glClear(GL_STENCIL_BUFFER_BIT);
-
-            glDrawArrays(GL_TRIANGLES, 36, 6);
-
-            // Draw cube reflection
-            glStencilFunc(GL_EQUAL, 1, 0xFF);
-            glStencilMask(0x00);
-            glDepthMask(GL_TRUE);
-
-            model = glm::scale(glm::translate(model, glm::vec3(0, 0, -1)), glm::vec3(1, 1, -1));
-            glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
-
-            glUniform3f(uniColor, 0.3f, 0.3f, 0.3f);
-                glDrawArrays(GL_TRIANGLES, 0, 36);
-            glUniform3f(uniColor, 1.0f, 1.0f, 1.0f);
-
-        glDisable(GL_STENCIL_TEST);
-
-        // Bind default framebuffer and draw contents of our framebuffer
-        glBindFramebuffer(GL_FRAMEBUFFER, gaussBlurFrameBuffer);
-        glBindVertexArray(vaoQuad);
-        glDisable(GL_DEPTH_TEST);
-        glUseProgram(gaussBlurShaderProgram);
-
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, sceneRenderTexColorBuffer);
-
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-
-        // Bind default framebuffer and draw contents of our framebuffer
-        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        glBindVertexArray(vaoQuad);
-        glDisable(GL_DEPTH_TEST);
-        glUseProgram(screenShaderProgram);
-
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, gaussBlurTexColorBuffer);
-
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-
-
-        // Swap buffers
-        window.display();
-
-        sf::sleep(sf::milliseconds(1)); // delay to avoid cpu overheating
+    for (unsigned int i = 0; i < 5; ++i) {
+        std::cout << feedback[i] << std::endl;
     }
 
-    glDeleteRenderbuffers(1, &sceneRenderRboDepthStencil);
-    glDeleteTextures(1, &sceneRenderTexColorBuffer);
-    glDeleteFramebuffers(1, &sceneRenderFrameBuffer);
+    // Cleanup
+    glDeleteBuffers(1, &tbo);
 
-    glDeleteRenderbuffers(1, &gaussBlurRboDepthStencil);
-    glDeleteTextures(1, &gaussBlurTexColorBuffer);
-    glDeleteFramebuffers(1, &gaussBlurFrameBuffer);
+    glDeleteProgram(program);
+    glDeleteShader(shader);
 
-    glDeleteTextures(1, &texKitten);
-    glDeleteTextures(1, &texPuppy);
+    glDeleteBuffers(1, &vbo);
 
-    glDeleteProgram(screenShaderProgram);
-    glDeleteShader(screenFragmentShader);
-    glDeleteShader(screenVertexShader);
-
-    glDeleteProgram(gaussBlurShaderProgram);
-    glDeleteShader(gaussBlurFragmentShader);
-    glDeleteShader(gaussBlurVertexShader);
-
-    glDeleteProgram(sceneShaderProgram);
-    glDeleteShader(sceneFragmentShader);
-    glDeleteShader(sceneVertexShader);
-
-    glDeleteBuffers(1, &vboCube);
-    glDeleteBuffers(1, &vboQuad);
-
-    glDeleteVertexArrays(1, &vaoCube);
-    glDeleteVertexArrays(1, &vaoQuad);
-
-    window.close();
+    glDeleteVertexArrays(1, &vao);
 
     return 0;
 }
